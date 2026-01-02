d13466d8c0652577a0dc7976c4bf31da8e5d7522
Kendi J <kendyjaky@gmail.com>
2026-01-02 19:52:55 +0100

    Initial Commit

---
+++ house_plants/house_plantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  house_plantsApp.swift
+//  house_plants
+//
+//  Created by Kendi J on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct house_plantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ house_plantsTests/house_plantsTests.swift
@@ -0,0 +1,17 @@
+//
+//  house_plantsTests.swift
+//  house_plantsTests
+//
+//  Created by Kendi J on 02/01/2026.
+//
+
+import Testing
+@testable import house_plants
+
+struct house_plantsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
