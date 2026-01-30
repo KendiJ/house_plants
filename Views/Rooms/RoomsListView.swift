46bcb389c6d00f1a109f8265c79e968481cbab1c
Kendi J <kendyjaky@gmail.com>
2026-01-30 22:58:18 +0100

    Cards implementation

--- Views/Rooms/RoomsListView.swift
+++ Views/Rooms/RoomsListView.swift
@@ -5,6 +5,11 @@ struct RoomsListView: View {
     // @StateObject means: "I own this object, please keep it alive."
     @StateObject private var viewModel = RoomViewModel()
     
+    let columns = [
+            GridItem(.flexible(), spacing: 16),
+            GridItem(.flexible(), spacing: 16)
+        ]
+    
     var body: some View {
         NavigationStack {
             Group {
@@ -21,13 +26,25 @@ struct RoomsListView: View {
                         }
                     }
                 } else {
-                    List(viewModel.rooms) { room in
-                        NavigationLink(destination: PlantsListView(roomName: room.name, roomID: room.id)) {
-                            RoomRowView(room: room)
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 16) {
+                            ForEach(viewModel.rooms) {room in
+                                NavigationLink(destination: PlantsListView(roomName: room.name, roomID: room.id)) {
+                                    RoomCardView(room: room)
+                                }
+                                .buttonStyle(.plain)
+                            }
                         }
                     }
+//
+//                    List(viewModel.rooms) { room in
+//                        NavigationLink(destination: PlantsListView(roomName: room.name, roomID: room.id)) {
+//                            RoomRowView(room: room)
+//                        }
+//                    }
                 }
             }
+            .padding()
             .navigationTitle("My Home")
             .task {
                 await viewModel.fetchRooms()
